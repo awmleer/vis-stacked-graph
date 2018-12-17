289dd9cdb0d44edd6ad5d2000c7b03e3460b28bd
awmleer <awmleer@sparker.xyz>
2018-12-17 10:27:25 +0800

    reduce wiggle

--- src/main.js
+++ src/main.js
@@ -1,50 +1,76 @@
 let data = [
-  [100, 30, 20, 50, 20, 60],
-  [30, 20, 10, 40, 0, 10],
+  [100, 30, 20, 50, 20, 60, 40, 10, 0, 20],
+  [30, 20, 10, 40, 0, 10, 20, 50, 30, 80],
+  [0, 0, 20, 30, 20, 10, 0, 30, 80, 10],
+  [50, 50, 20, 30, 80, 20, 10, 50, 0, 100],
 ];
 
-let x = [0, 20, 40, 60, 80, 100];
+let x = [0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
 
 const colors = [
   '#00aa00',
   '#aa0000',
+  '#0000aa',
+  '#333333',
+  '#ffffff',
 ]
 
 let canvas = document.getElementById('stacked-graph');
 let ctx = canvas.getContext('2d');
-// ctx.fillStyle = 'rgb(200, 0, 0)';
-// ctx.fillRect(10, 10, 50, 50);
 
-// ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-// ctx.fillRect(30, 30, 50, 50);
+ctx.translate(0, canvas.height/2);
+ctx.scale(1, -1);
 
-data.forEach((points, index) => {
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  ctx.lineTo(x[0], points[0]);
-  let i;
-  for (i = 1; i < points.length - 2; i ++) {
-    var xc = (x[i] + x[i+1]) / 2;
-    var yc = (points[i] + points[i + 1]) / 2;
-    ctx.quadraticCurveTo(x[i], points[i], xc, yc);
+
+function draw(data) {
+  data.forEach((points, index) => {
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height/2 + 1);
+    ctx.lineTo(x[0], points[0]);
+    let i;
+    for (i = 1; i < points.length - 2; i ++) {
+      var xc = (x[i] + x[i+1]) / 2;
+      var yc = (points[i] + points[i + 1]) / 2;
+      ctx.quadraticCurveTo(x[i], points[i], xc, yc);
+    }
+    ctx.quadraticCurveTo(x[i], points[i], x[i+1], points[i+1]);
+    ctx.lineTo(x[i+1], canvas.height/2 + 1);
+    ctx.closePath();
+    ctx.fillStyle = colors[index];
+    ctx.fill();
+    ctx.stroke();
+  })
+}
+
+function reduceWiggle(data) {
+  const n = data.length;
+  const g0 = [];
+  let g = 0;
+  for (let x = 0; x < data[0].length; x++) {
+    data.forEach((points, index) => {
+      const i = index + 1;
+      g += (n - i + 1) * points[x];
+    })
+    g = -g / (n+1);
+    g0.push(g);
   }
-  ctx.quadraticCurveTo(x[i], points[i], x[i+1], points[i+1]);
-  ctx.lineTo(x[i+1], 0);
-  ctx.closePath();
-  ctx.fillStyle = colors[index];
-  ctx.fill();
-  ctx.stroke();
-})
+  return g0;
+}
+
+function normalize(data) {
+  const g0 = reduceWiggle(data);
+  console.log(g0);
+  const normalizedData = [ g0 ];
+  data.forEach((points, i) => {
+    normalizedData[i+1] = [];
+    points.forEach((point, j) => {
+      normalizedData[i+1][j] = normalizedData[i][j] + point;
+    })
+  })
+  return normalizedData;
+}
 
 
-// ctx.moveTo(x[0], data[0][0])
-// ctx.lineTo(x[0], data[1][0])
-// ctx.moveTo(x[5], data[0][5])
-// ctx.lineTo(x[5], data[1][5])
+const normalizedData = normalize(data);
 
-
-
-// ctx.beginPath();
-// ctx.moveTo(0, 0);
-// ctx.quadraticCurveTo(20, 100, 200, 20);
-// ctx.stroke();
+draw(normalizedData);
